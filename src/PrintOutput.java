ced6185644a557cdf34fb0b9ca8d1dc513dd341a
abdullahshahad <abdullahshahad.w@gmail.com>
2025-05-20 22:40:50 +0600

    print staments done

---
+++ src/PrintOutput.java
@@ -0,0 +1,14 @@
+public class PrintOutput {
+    public static void main(String[] args) {
+        System.out.print("Hello World\n");
+        System.out.println("Hello World");
+
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
+    }
+}
---
+++ src/PrintZ.java
@@ -0,0 +1,17 @@
+public class PrintZ {
+    public static void main(String[] args) {
+        System.out.println("* * * * *");
+        System.out.println("      *");
+        System.out.println("    *");
+        System.out.println("  *");
+        System.out.println("* * * * *");
+
+//        How shown in the video
+
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}
